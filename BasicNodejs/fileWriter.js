7c2e3e7f375fe92e9ce95ba6bd951eca72651fdb
Onur Gündoğdu <gundogduonur@outlook.com.tr>
2024-09-02 19:21:07 +0300

    example write file

---
+++ BasicNodejs/fileWriter.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.writeFile('example.txt', 'Hello World!', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Written file');
+  });
